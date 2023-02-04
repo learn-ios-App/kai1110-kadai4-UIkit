1c08bdb868c114987f78d77db13a91b1d036917d
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-04 22:28:51 +0900

    commit

--- kai1110-kadai4-UIkit/ViewController.swift
+++ kai1110-kadai4-UIkit/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  kai1110-kadai4-UIkit
-//
-//  Created by 渡邊魁優 on 2023/02/04.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var numberLabel: UILabel!
+    private var number = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        upDataLabel()
     }
 
+    @IBAction func plus(_ sender: Any) {
+        number += 1
+        upDataLabel()
+    }
 
+    @IBAction func clear(_ sender: Any) {
+        number = 0
+        upDataLabel()
+    }
+    //値の更新
+    private func upDataLabel() {
+        numberLabel.text = String(number)
+    }
 }
 
